64c2328b0b2516e46460dcba5a32007d5e03f5c8
SINDHUJA C S <sindhujacs1504@gmail.com>
2026-04-23 23:30:52 +0530

    Enhance HelloServlet to return HTML response
    Updated the doGet method to set content type and return HTML response.

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -5,6 +5,14 @@ import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.getWriter().println("Success");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>CI/CD Pipeline Successful 🚀</h1>");
+        out.println("<p>Hello from Tomcat Servlet</p>");
+        out.println("</body></html>");
     }
 }
