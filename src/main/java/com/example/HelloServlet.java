5c75e649c67ff9bc5b423942e4747e2764404401
SINDHUJA C S <sindhujacs1504@gmail.com>
2026-04-23 23:53:57 +0530

    Refactor HelloServlet to return simple success message
    Simplified response in HelloServlet to return 'Success'.

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -5,14 +5,6 @@ import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-
-        res.setContentType("text/html");
-
-        PrintWriter out = res.getWriter();
-
-        out.println("<html><body>");
-        out.println("<h1>CI/CD Pipeline Successful 🚀</h1>");
-        out.println("<p>Hello from Tomcat Servlet</p>");
-        out.println("</body></html>");
+        res.getWriter().println("Success");
     }
 }
